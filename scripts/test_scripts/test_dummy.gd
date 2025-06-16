95ed6b4dc32e430c0bbcaf70ed44838db6365efb
Cristian <cristianpmason@icloud.com>
2025-06-16 18:15:42 -0400

    s

--- scripts/test_scripts/test_dummy.gd
+++ scripts/test_scripts/test_dummy.gd
@@ -10,6 +10,7 @@ func on_dead(nam: String) -> void:
 	if get_tree().get_first_node_in_group("enemy").name == nam:
 		velocity = Vector2.ZERO
 		stats.isDead = true
+		print("dead")
 		#play death animation
 		#queue_free()
 	else:
