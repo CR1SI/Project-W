44551fba8ac764cc1b9e489fe26c357957f60443
Cristian <cristianpmason@icloud.com>
2025-06-23 19:47:34 -0400

    sss

--- scripts/test_scripts/test_dummy.gd
+++ scripts/test_scripts/test_dummy.gd
@@ -23,28 +23,26 @@ func on_dead(nam: String) -> void:
 		return
 
 func _physics_process(delta: float) -> void:
+	
+	#TODO Make navigation its own object to drag and drop on enemies!
 	if nav.is_navigation_finished() or stats.isDead:
 		velocity = Vector2.ZERO
 		move_and_slide()
 		return
-	
 	if target != null and is_instance_valid(target):
 		nav.set_target_position(target.global_position)
-	
 	#get position to next node
 	var next_pos: Vector2 = nav.get_next_path_position()
 	#calc direction
 	var direction: Vector2 = (next_pos - global_position).normalized()
 	#set velocity and move
 	velocity = velocity.lerp((direction).normalized() * stats.speed, stats.acceleration * delta)
-	
 	move_and_slide()
 
 func _on_target_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		target = body
 		nav.set_target_position(body.global_position)
-
 
 func _on_target_area_body_exited(body: Node2D) -> void:
 	if body == target:
